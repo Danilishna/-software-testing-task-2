a061dd2c3f4a2a04a0bf3ca66acf529c8035a722
unknown <C:\Users\BIT\AppData\Roaming\The Bat!>
2020-04-30 10:52:22 +0300

    first edition

---
+++ src/main/java/org/example/Matrix.java
@@ -0,0 +1,86 @@
+package org.example;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Matrix {
+    public static double [][] addition(double [][] first, double [][] second) throws Exception {
+        if (first.length != second.length || first[0].length != second[0].length)
+            throw new Exception();
+        double[][] res = new double[first.length][second[0].length];
+        for (int i = 0; i < first.length; i++)
+            for (int j = 0; j < first[0].length; j++)
+                res[i][j] = first[i][j] + second[i][j];
+        return res;
+    }
+    public static double [][] substraction(double [][] first, double [][] second) throws Exception {
+        if (first.length != second.length || first[0].length != second[0].length)
+            throw new Exception();
+        double[][] res = new double[first.length][second[0].length];
+        for (int i = 0; i < first.length; i++)
+            for (int j = 0; j < first[0].length; j++)
+                res[i][j] = first[i][j] - second[i][j];
+        return res;
+    }
+    public static double [][] addValue(double [][] first, double value) {
+        if (value == 0)
+            return first;
+        double[][] res = new double[first.length][first[0].length];
+        for (int i = 0; i < first.length; i++)
+            for (int j = 0; j < first[0].length; j++)
+                res[i][j] = first[i][j] + value;
+        return res;
+    }
+    public static double[][] readMatrixFromFile(String file) throws Exception {
+        FileReader fileReader = new FileReader("./src/main/java/resources/" + file);
+        Scanner scanner = new Scanner(fileReader);
+
+        List<Double> matrix = new ArrayList<>();
+
+        int row = 0, col = 0;
+        while(scanner.hasNextLine()) {
+            col = 0;
+            Scanner rowScanner = new Scanner(scanner.nextLine());
+            while(rowScanner.hasNextDouble()) {
+                matrix.add(rowScanner.nextDouble());
+                col++;
+            }
+            row++;
+        }
+        double[][] result = new double[row][col];
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                result[r][c] = matrix.get(r * col + c);
+            }
+        }
+        fileReader.close();
+        return result;
+    }
+    public static double[][] multiply(double[][] firstMatrix, double[][] secondMatrix) {
+        double[][] result = new double[firstMatrix.length][secondMatrix[0].length];
+        for (int row = 0; row < result.length; row++) {
+            for (int col = 0; col < result[row].length; col++) {
+                result[row][col] = multiplyMatricesCell(firstMatrix, secondMatrix, row, col);
+            }
+        }
+        return result;
+    }
+    private static double multiplyMatricesCell(double[][] firstMatrix, double[][] secondMatrix, int row, int col) {
+        double cell = 0;
+        for (int i = 0; i < secondMatrix.length; i++) {
+            cell += firstMatrix[row][i] * secondMatrix[i][col];
+        }
+        return cell;
+    }
+    public static void print(double[][] matrix) {
+        int rows=matrix.length;
+        int cols=matrix[0].length;
+        for(int i=0;i<rows;i++) {
+            for(int j=0;j<cols;j++)
+                System.out.print(matrix[i][j]+" ");
+            System.out.println();
+        }
+    }
+}
---
+++ src/test/java/org/example/MatrixTest.java
@@ -0,0 +1,127 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MatrixTest {
+
+    private Matrix mtr;
+
+    @BeforeEach
+    public void initMatrix() {
+        mtr = new Matrix();
+    }
+
+    @AfterEach
+    public void deleteMatrix() {
+        mtr = null;
+    }
+
+    @Test
+    public void TestMultiply() {
+        double [][] first = {{1, 2}, {3, 4}};
+        double [][] second = {{1, 2}, {3, 4}};
+        double [][] res = {{7, 10}, {15, 22}};
+        double [][] actual_res = mtr.multiply(first, second);
+        assertArrayEquals(res, actual_res);
+    }
+
+    @Test
+    public void TestAddition() throws Exception {
+        double [][] first = {{1, 2}, {3, 4}};
+        double [][] second = {{1, 2}, {3, 4}};
+        double [][] res = {{2, 4}, {6, 8}};
+        double [][] actual_res = mtr.addition(first, second);
+        assertArrayEquals(res, actual_res);
+    }
+
+    @Test()
+    public void TestAdditionWithException() throws Exception {
+        double [][] first = {{1, 2}, {3, 4}};
+        double [][] second = {{1, 2}, {3, 4}, {5, 6}};
+        assertThrows(Exception.class, () -> mtr.addition(first, second));
+    }
+
+    @Test()
+    public void TestAdditionWithException2() throws Exception {
+        double [][] first = {{1, 2, 3}, {3, 4, 5}};
+        double [][] second = {{1, 2}, {3, 4},};
+        assertThrows(Exception.class, () -> mtr.addition(first, second));
+    }
+
+    @Test
+    public void TestSubstraction() throws Exception {
+        double [][] first = {{1, 2}, {3, 4}};
+        double [][] second = {{1, 2}, {3, 4}};
+        double [][] res = {{0, 0}, {0, 0}};
+        double [][] actual_res = mtr.substraction(first, second);
+        assertArrayEquals(res, actual_res);
+    }
+
+    @Test()
+    public void TestSubstractionWithException() throws Exception {
+        double [][] first = {{1, 2}, {3, 4}};
+        double [][] second = {{1, 2}, {3, 4}, {5, 6}};
+        assertThrows(Exception.class, () -> mtr.substraction(first, second));
+    }
+
+    @Test()
+    public void TestSubstractionWithException2() throws Exception {
+        double [][] first = {{1, 2, 3}, {3, 4, 5}};
+        double [][] second = {{1, 2}, {3, 4},};
+        assertThrows(Exception.class, () -> mtr.substraction(first, second));
+    }
+
+    @Test
+    public void TestAddValue0() {
+        double [][] first = {{1, 2}, {3, 4}};
+        int val = 0;
+        Matrix a = new Matrix();
+        double [][] actual_res = a.addValue(first, val);
+        assertArrayEquals(first, actual_res);
+    }
+
+    @Test
+    public void TestAddValue() {
+        double [][] first = {{1, 2}, {3, 4}};
+        int val = 10;
+        double [][] res = {{11, 12}, {13, 14}};
+        Matrix a = new Matrix();
+        double [][] actual_res = a.addValue(first, val);
+        assertArrayEquals(res, actual_res);
+    }
+
+    @Test
+    public void TestReadMatrixFromFile() throws Exception {
+        double[][] expect_matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        assertArrayEquals(expect_matrix, mtr.readMatrixFromFile("matrix1"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"matrix1", "matrix2", "matrix3"})
+    public void TestReadMatrixFromFileWithNoThrow(String candidate) throws Exception {
+        assertDoesNotThrow(() -> Matrix.readMatrixFromFile(candidate));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"3","4","5"})
+    public void TestAddValueToZeroMatrix(double a) {
+        double[][] zeros = {{0, 0},{0, 0}};
+        double[][] actual = new double[zeros.length][zeros[0].length];
+        for (int i = 0; i < zeros.length; i++)
+            for(int j = 0; j < zeros[0].length; j++)
+                actual[i][j] = a;
+        double[][] exp = mtr.addValue(zeros, a);
+        assertArrayEquals(exp, actual);
+    }
+}
